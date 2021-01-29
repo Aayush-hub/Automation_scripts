dbf2c78c4f4c9c5feb02970b65d8aab2e8f298a4
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2021-01-29 15:59:22 +0530

    Add files via upload

---
+++ insta_automator.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from getpass import getpass
+import time
+chrome = webdriver.Chrome(ChromeDriverManager().install())
+chrome.get("https://instagram.com")
+time.sleep(4)
+username = chrome.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
+username.send_keys("ayushgarg1951")
+password = chrome.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
+passw = getpass()
+password.send_keys(passw)
+time.sleep(2)
+login_btn = chrome.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div')
+login_btn.click()
+# notn = chrome.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button')
+# notn.click()
+# newn = chrome.find_element_by_xpath('/html/body/div[3]/div/div/div/div[3]/button[2]')
+# newn.click()
