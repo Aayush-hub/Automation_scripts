876aa6f6349c3dc2a471242e81a282e0aa9e44dd
Aayush Garg <ayushgarg1951@gmail.com>
2021-02-03 13:38:54 +0530

    Update dinogame.py

--- dinogame.py
+++ dinogame.py
@@ -25,3 +25,20 @@ if __name__ == "__main__":
         image = ImageGrab.grab().convert('L')  
         data = image.load()
         isCollide(data)
+        
+        '''
+        Code for testing co-ordinates
+        
+        # Draw the rectangle for cactus
+        for i in range(200, 300):
+            for j in range(390, 450):
+                data[i, j] = 0
+        
+        # Draw the rectangle for birds
+        for i in range(250, 300):
+            for j in range(410, 563):
+                data[i, j] = 171
+
+        image.show()
+        break
+        '''
